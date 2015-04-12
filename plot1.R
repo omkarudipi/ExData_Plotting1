858065479859b214f7673312793163e124127819
omkarudipi <omkarudipi@gmail.com>
2015-04-13 01:33:26 +0800

    plot1 commit
    plot1 commit

---
+++ plot1.R
@@ -0,0 +1,25 @@
+#assumes working directory is set
+
+#read whole source table into dataframe assign1
+assign1 <- read.table("household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+
+#copy subset of assign1 (1/2/2007 and 2/2/2007) to assign1sub
+assign1sub <- assign1[which(assign1$Date=="1/2/2007" | assign1$Date=="2/2/2007"),]
+
+#update values in Date column to convert string to Date
+assign1sub$Date <- as.Date(assign1sub$Date, "%d/%m/%Y")
+
+#update values in Time column to convert string to Time
+assign1sub$Time <- strptime(
+    paste(assign1sub$Date, assign1sub$Time, sep=" "), 
+    "%Y-%m-%d %H:%M:%S")
+
+
+#update values in Global_active_power column to convert string to numeric
+assign1sub$Global_active_power <- as.numeric(assign1sub$Global_active_power)
+
+#generate plot1.png file, compare with assignment
+png("plot1.png", width=480, height=480)
+hist(assign1sub$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
+
